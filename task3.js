7b0672bb7f9b0dac876c4d39e8d236491e61bc79
Menna <mennaelhosiny>
2023-01-28 22:43:20 +0200

    updated

--- task3.js
+++ task3.js
@@ -1,6 +1,7 @@
 let input =document.getElementById("input")
 let btn=document.getElementById("btn")
 let answer =document.getElementById('answer')
+let count=3
 
 let Rnum =Math.floor(Math.random() * 9) + 1
 console.log(Rnum);
@@ -10,5 +11,15 @@ btn.addEventListener('click',() => {
     console.log(valueOfnum)
     if (valueOfnum===Rnum)
      answer.innerHTML='Well Done :)'
-    else answer.innerHTML='Try Again :]'
+    if (valueOfnum!==Rnum)
+    { answer.innerHTML='Try Again :]'
+         if (count>=0,count--)
+         document.getElementById("boxx").innerHTML=count
+}
+if (count==0) {
+    answer.innerHTML='Game Over :]'
+    setTimeout(function(){location.reload()},4000)
+
+}
 })
+
