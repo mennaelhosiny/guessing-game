02a6ebc64820efc1f1338328c1fae71088c4c05d
Menna <mennaelhosiny>
2023-01-30 02:26:40 +0200

    updated

--- task3.js
+++ task3.js
@@ -1,25 +1,42 @@
-let input =document.getElementById("input")
-let btn=document.getElementById("btn")
-let answer =document.getElementById('answer')
-let count=3
+const input =document.getElementById("input")
+const btn=document.getElementById("btn")
+const answer =document.getElementById('answer')
+let count=2
+const boxx=document.querySelector(".box")
+const game=document.querySelector('.guessing-game')
 
-let Rnum =Math.floor(Math.random() * 9) + 1
+const Rnum =Math.round(Math.random() * 10) + 1
 console.log(Rnum);
 btn.addEventListener('click',() => {
-    let values =input.value
-    let valueOfnum = Number (values)
+    answer.classList.remove("hidden")
+    const values =input.value
+    const valueOfnum = Number(values)
     console.log(valueOfnum)
-    if (valueOfnum===Rnum)
-     answer.innerHTML='Well Done :)'
-    if (valueOfnum!==Rnum)
+    if (count>0 && valueOfnum===Rnum){
+        answer.innerHTML='Well Done :)'
+        disable();
+    }
+    
+   else if (count!=0)
     { answer.innerHTML='Try Again :]'
-         if (count>=0,count--)
-         document.getElementById("boxx").innerHTML=count
+      boxx.innerHTML=boxx.innerHTML-1
+      disable();
+      count--;
+       
 }
-if (count==0) {
+else if (count==0) {
     answer.innerHTML='Game Over :]'
+    boxx.innerHTML=boxx.innerHTML-1
+
+    disable();
+    game.style.border ='2px solid #ff0000'
     setTimeout(function(){location.reload()},4000)
 
 }
 })
+function disable(){
+    input.value="";
+    input.classList.add('disable');
+    btn.classList.add('disable');
+}
 
